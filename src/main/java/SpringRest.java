ec2f810fadf68d7596bfb778300cd57f8e88113e
Andrei Sharapov <adras.sad@gmail.com>
2023-06-16 14:18:26 +0200

    11

--- src/main/java/SpringRest.java
+++
@@ -1,9 +0,0 @@
-//import org.springframework.boot.auto
-
-public class SpringRest {
-
-  //  @SpringBootApplication
-    public static void main(String[]args){
-
-    }
-}
---
+++ src/main/java/adrassad/ParserJSONApp.java
@@ -0,0 +1,11 @@
+package adrassad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserJSONApp {
+    public static void main(String[] args){
+        SpringApplication.run(ParserJSONApp.class);
+    }
+}
